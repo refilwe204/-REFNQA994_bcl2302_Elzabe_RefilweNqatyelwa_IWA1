463a190b3cba656f474ffb4959a1e2a168a5dfa8
refilwe204 <nqatyelwaangelin@gmail.com>
2023-03-30 11:44:59 +0200

    second edits to the code

--- IWA4/scripts.js
+++ IWA4/scripts.js
@@ -2,24 +2,23 @@ let date =2050;
 let status = 'student';
 let count = 0;
 
-if (date = 2050) {
-	console.log("January", 'New Year’s Day'
-	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	let count = count + 4
+if (date === 2050) { 
+	console.log("January", 'New Year’s Day');
+	console.log("March", 'Human Rights Day');
+	console.log("April", 'Family Day'); 
+	console.log("April", 'Freedom Day'); 
+	count += 4; 
 
-	if (status = "student") {
-	  console.log('June', 'Youth Day')
-		let count = count + 1
-  }
 
-	console.log('August', 'Women’s Day')
-	console.log('September', 'Heritage Day')
-	date = 'December'
-	console.log(date, 'Day of Reconciliation')
-	let count = count + 3
+	if (status === "student") {
+		console.log('June', 'Youth Day');
+		count += 1;
+	}
+
+	console.log('August', 'Women’s Day');
+	console.log('September', 'Heritage Day');
+	console.log('December', 'Day of Reconciliation');
+	count += 3;
 
 	if (status = "parent") {
 	  console.log(date, 'Christmas Day')
