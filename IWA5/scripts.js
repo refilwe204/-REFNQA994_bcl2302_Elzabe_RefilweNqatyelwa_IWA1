c786f780c04a5446a3b310e13d17894f10194355
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-02 22:31:20 +0200

    edits to console.log the amount specified

--- IWA5/scripts.js
+++ IWA5/scripts.js
@@ -1,40 +1,45 @@
-const FREE_WARNING = 'Free shipping only applies to single customer orders';
+let FREE_WARNING = 'Free shipping only applies to single customer orders';
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = '0';
 
-const shippingCosts = {
-  'RSA': 400,
-  'NAM': 600,
-  'NK': null,
-  'default': 800
-};
 
-const currencySymbols = {
-  'RSA': 'R',
-  'NAM': '$',
-  'default': '$'
-};
-
-const subtotal = 300 + 100 * 5 + 150 * 0 + 35 * 2 + 5 * 0; // calculate subtotal based on item prices
-
-let shipping = null;
-let currency = null;
-let location = 'RSA';
-let customers = 1;
-
-if (location === 'NK') {
-  console.log(BANNED_WARNING);
-} else {
-  shipping = shippingCosts[location] || shippingCosts['default'];
-  currency = currencySymbols[location] || currencySymbols['default'];
-  
-  if (subtotal >= 1000 && customers === 1 && (location === 'RSA' || location === 'NAM')) {
-    shipping = 0;
-  } else if (shipping === 0 && customers !== 1) {
-    console.log(FREE_WARNING);
-  }
-  
-  console.log(`Price: ${currency}${subtotal + shipping}`);
+const userShippingcost = () => {
+    if (location === "RSA") { 
+    let userCost = 0
+    let user = 0
+      console.log(shipping === 400 && currency === 'R')
+      if(userCost >= 1000 && user > 1 ) {
+      console.log(shipping = 0)
+     }
+    }
+    if (location = "NK"){
+        console.log(BANNED_WARNING)
+        }
 }
+
+const checkUserCost = () => {
+if (location = "NAM"){
+    let userCost = 0
+    let user = 0
+    console.log(shipping = 600 && currency === '$')
+    if(userCost >= 1000 && user > 1 ) {
+       console.log(shipping = 0)
+    }
+   }
+}
+
+{
+
+
+shoes = 300 * 1;
+toys = 100 * 5;
+batteries = 35 * 2;
+let price = 0;
+const userLocation =  (location === "RSA")
+const calculatePrice = userLocation ? console.log(BANNED_WARNING) : console.log("Price", price = `${shoes + toys + batteries + 400}`)
+
+}
+
 
 
 
