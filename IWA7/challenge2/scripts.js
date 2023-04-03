72834c99c6a9219a941d930505cd377eb9dda0a9
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-03 22:00:40 +0200

    first edits to scripts of challenge 7.2

---
+++ IWA7/challenge2/scripts.js
@@ -0,0 +1,10 @@
+const nickname= "Timmy";
+const firstName = "Timothy";
+
+if (nickname) {
+  console.log(`Good Morning, ${nickname}!`);
+} else if (firstName) {
+  console.log(`Good Morning, ${firstName}!`);
+} else {
+  console.log("Good Morning!");
+}
