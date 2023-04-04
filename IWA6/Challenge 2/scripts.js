32990fe27ddaa5f056c54cd62022bf8362937e9e
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-04 13:38:28 +0200

    edits to IWA7 1 & 2

--- IWA6/Challenge 2/scripts.js
+++ IWA6/Challenge 2/scripts.js
@@ -10,13 +10,13 @@ const minuteOfDay = 0;
 
 // Check if it's midnight and if minuteOfDay has been set
 // Also check if hourOfDay is 0 and minuteOfDay is 0
-if (hourOfDay === 0 && minuteOfDay !== null && hourOfDay == 0 && minuteOfDay == 0 ) {
+if (hourOfDay !== undefined && minuteOfDay !== undefined && hourOfDay === 0 && minuteOfDay === 0) {
  
  // Convert tax to a decimal
   const taxAsDecimal = parseInt(tax) / 100;
 
 // Calculate salary after tax
-  const startingAfterTax = salary - salary * taxAsDecimal;
+  const startingAfterTax = salary *(1- taxAsDecimal);
 
 // Calculate balance after deducting transport, food, and rent
   const balance = startingAfterTax - transport - food - rent;
