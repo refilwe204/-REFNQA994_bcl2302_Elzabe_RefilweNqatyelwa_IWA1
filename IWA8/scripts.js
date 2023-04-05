09fc07df70cc4d45295001e41f9e57fb9b175684
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-05 22:14:24 +0200

    edits and comments to the code

--- IWA8/scripts.js
+++ IWA8/scripts.js
@@ -26,7 +26,9 @@ const leo = {
 };
 
 const sarah = {
-	name: $(sarahName) + $(sarahSurname),
+	// trim to close the space all together
+	// double quotes to create a single white space
+	name: sarahName.trim() + " " + sarahSurname,
 	balance: sarahBalance,
     accessId: '6b279ae5-5657-4240-80e9-23f6b635f7a8',
     age: 62,
@@ -35,7 +37,7 @@ const sarah = {
 		street: sarahStreet,
 		postalCode: sarahPostal,
 	},
-},
+};
 
 
 console.log(leo);
