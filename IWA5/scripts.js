6427bfc15a871c3a8134ca2c918e8c9bc38c7062
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-05 09:32:00 +0200

    commit

--- IWA5/scripts.js
+++ IWA5/scripts.js
@@ -6,40 +6,50 @@ const NONE_SELECTED = '0';
 // calculating shipping cost based on user's location
 const userShippingCost = () => {
     if (location === "RSA") { 
-    let userCost = 0 
-    let user = 0
-      console.log(shipping === 400 && currency === 'R') // Log a message if shipping is 400 and currency is R
-      if(userCost >= 1000 && user > 1 ) { // If userCost is >= 1000 and user is > 1
-      console.log(shipping = 0) // Set shipping to 0
+    console.log(shipping === 400 && currency === 'R') // Log a message if shipping is 400 and currency is R
+    if(userCost >= 1000 && user == 1 ) {
+        userShippingCost = 0
+    }
+     {
+        elseif(userCost >=1000 && user >= 1)
+        console.log(BANNED_WARNING)
      }
-    }
-    if (location = "NK"){ 
+ }
+       else(location = "NK") 
         console.log(BANNED_WARNING) // Log a message that we do not ship to this country
-    }
-        }
+}
  
 
 // Function to check user's cost based on location 
 const checkUserCost = () => {
 if (location = "NAM"){
-    let userCost = 0
-    let user = 0
     console.log(shipping = 600 && currency === '$')
-    if(userCost >= 1000 && user > 1 ) {
-       console.log(shipping = 0)
+    if(userCost >= 1000 && user == 1 ) {  
+        userShippingCost = 0 
     }
-   }
+    {
+        elseif( userCost >=1000 && user >= 1)
+        console.log(BANNED_WARNING)
+     }  
+  }
+
 }
 
+
+
 {
-
-
+// Set the prices for shoes, toys, and batteries
 shoes = 300 * 1;
 toys = 100 * 5;
 batteries = 35 * 2;
-let price = 0;
+shipping = 400; // Set the shipping cost
+let price = 0; // Initialize the price variable to 0
+
+// Determine if the user is located in South Africa (RSA)
 const userLocation =  (location === "RSA")
-const calculatePrice = userLocation ? console.log(BANNED_WARNING) : console.log("Price", price = `${shoes + toys + batteries + 400}`)
+
+//calculate the price and log it to the console
+console.log("Price", price = `${shoes + toys + batteries +shipping}`)
 
 }
 
