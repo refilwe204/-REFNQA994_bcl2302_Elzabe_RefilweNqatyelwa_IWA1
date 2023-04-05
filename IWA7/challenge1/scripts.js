f54616ec43d528524ebd6970997793b47e859668
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-05 20:18:56 +0200

    fixing files

---
+++ IWA7/challenge1/scripts.js
@@ -0,0 +1,7 @@
+// Define a variable `value` and assign it the string value "3"
+const value = "3";
+
+// Use the `parseInt()` function to convert `value` to a numerical value, and add it to the numerical value 4.
+// Then use `parseInt()` again to convert `value` back to a numerical value, and add it to the result of the previous addition.
+// Log the result of this addition to the console.
+console.log(parseInt(value) + 4 + parseInt(value));
