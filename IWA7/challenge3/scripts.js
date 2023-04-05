613e788498f3aaf982b77fc57d29ff2ad48bc560
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-05 15:23:58 +0200

    comments to the code

--- IWA7/challenge3/scripts.js
+++ IWA7/challenge3/scripts.js
@@ -14,15 +14,16 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-
+// Calculate the total amount owed
 const owed =  (Math.abs(leoBalance) + Math.abs(sarahBalance)).toFixed(2)
-
+//  string for Leo's information, with the name, surname, and balance
 const leo = `${leoName} ${leoSurname.trim()} (Owed: R ${Math.abs(leoBalance).toFixed(2)})`
-
+// string for Sarah's information, with the name, surname, and balance
 const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: R ${Math.abs(sarahBalance).toFixed(2)})`
-
+// string for the total amount owed
 const total = "  Total amount owed: R "
 
+// Combine the strings into a final output string
 const result = `${leo}\n${sarah}\n\n${divider}\n${total}${owed}\n${divider}`;
 
 console.log(result)
